f1704fa0dafd05525775fd5ade7b129427bbd466
Stormfall13 <85984394+Stormfall13@users.noreply.github.com>
2023-12-24 23:15:17 +0500

    asdasd

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -1,15 +1,21 @@
 import React, { useContext } from 'react';
 import { Context } from '../../index';
+import {observer} from "mobx-react-lite";
 
-const NavBar = () => {
+const NavBar = observer(() => {
 
     const {user} = useContext(Context)
 
     return (
         <div>
-            <button>Авторизация</button>
+            {user.isAuth ? 
+            <button>Войти</button>
+            :
+            <button onClick={() => user.setIsAuth(true)}>Авторизация</button>
+        }
+            
         </div>
     )
-}
+});
 
 export default NavBar;
--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,34 @@
 import React from 'react'
+import './auth.css';
+import { NavLink, useLocation } from 'react-router-dom';
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from '../utils/consts';
 
 const Auth = () => {
+
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE
+
+
   return (
     <div>
-      AUTH
+      <h2>{isLogin ? 'Авторизация' : 'Регистрация'}</h2>
+      <form className='auth'>
+        <input type="text" placeholder='Введите ваш email...'/>
+        <input type="text" placeholder='Введите ваш пароль...'/>
+        <div className="btn__wrapp">
+          {
+            isLogin ? 
+              <div className="register__wrapp">
+                Нет аккаунта ? <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйтесь</NavLink>
+              </div>
+              :
+              <div>
+                Есть аккаунт ? <NavLink to={LOGIN_ROUTE}>Войдите</NavLink>
+              </div>
+          }
+          <button>{isLogin ? 'Войти' : 'Регистрация'}</button>
+        </div>
+      </form>
     </div>
   )
 }
