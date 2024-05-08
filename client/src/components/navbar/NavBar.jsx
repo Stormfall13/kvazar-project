57bc552f517c54979860a26e3e85d6cbc864ce6e
Vyacheslav <stormfall228@gmai.com>
2024-05-08 17:55:57 +0500

    asdad

--- client/src/components/navbar/NavBar.jsx
+++ client/src/components/navbar/NavBar.jsx
@@ -13,41 +13,19 @@ const NavBar = observer(() => {
     const navigate = useNavigate();
     
 
-    // const logOut = () => {
-    //     user.setUser({})
-    //     user.setIsAuth(false)
-    //     localStorage.removeItem('token');
-    //     navigate(LOGIN_ROUTE)
-    // }
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        
-        
-        // Отправка данных на сервер
-        fetch('http://localhost:5000/api/user/logout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(item),
-            })
-            .then((response) => response.json())
-            .then((data) => {
-            // Обработка ответа от сервера
-            // console.log(data);
-            })
-            .catch((error) => {
-            // Обработка ошибки
-            console.error(error);
-        });
+    const logOut = () => {
+        user.setUser({})
+        user.setIsAuth(false)
+        localStorage.removeItem('token');
+        navigate(LOGIN_ROUTE)
     }
+
 
 
     return (
         <div>
             {user.isAuth ? 
-            <button onClick={handleSubmit}>Выйти</button>
+            <button onClick={logOut()}>Выйти</button>
             :
             // <button onClick={() => history.push(LOGIN_ROUTE)}>Авторизация</button>
             <button onClick={() => navigate(LOGIN_ROUTE)}>Авторизация</button>
